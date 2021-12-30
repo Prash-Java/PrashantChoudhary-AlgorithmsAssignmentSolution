73e299ba2beb7b81548a03e0a1214436a691e84b
Prashant <prash.click1@gmail.com>
2021-12-30 22:00:27 +0530

    formatting

--- src/com/greatlearning/stockersservicesversionone/MergeSortImplementation.java
+++ src/com/greatlearning/stockersservicesversionone/MergeSortImplementation.java
@@ -103,72 +103,75 @@ public class MergeSortImplementation {
 	/*AS WE CAN SEE BELOW COMMENTED CODE, WHICH IS FOR MERGE SORT IN DESCENDING ORDER,so to avoid this much almost redundant code, we controlled
 	using Flag value and incorporated Merge Sort in Ascending Order or Descending Order With Same Single Code Snippet as our PROBLEM STATEMENT
 	requires only one case mandatorily for Descending Order*/
+
 	//In case if we want to keep Merge Sort Methods as separate, we can very well use below code snippet and can classify these on sorting order
 
 	/*This method is belonging to service package and helps to sort an array, and in this case in descending order,
     This Uses Divide & Conquer Approach Of Merge Sort */
-	//	public void mergeForDescendingOrder(double[] stockPrices, int left, int mid, int right) {
-	//		// Find sizes of two sub arrays to be merged
-	//		int n1 = mid - left + 1;
-	//		int n2 = right - mid;
-	//
-	//		/* Create temporary arrays */
-	//		double leftArray[] = new double[n1];
-	//		double rightArray[] = new double[n2];
-	//
-	//		/* Copy data to temporary arrays */
-	//		for (int i = 0; i < n1; ++i)
-	//			leftArray[i] = stockPrices[left + i];
-	//		for (int j = 0; j < n2; ++j)
-	//			rightArray[j] = stockPrices[mid + 1 + j];
-	//
-	//		/* Merge the temporary arrays */
-	//
-	//		// Initial indexes of first and second sub-arrays
-	//		int i = 0, j = 0;
-	//
-	//		// Initial index of merged sub-array array
-	//		int k = left;
-	//		while (i < n1 && j < n2) {
-	//			if (leftArray[i] >= rightArray[j]) {
-	//				stockPrices[k] = leftArray[i];
-	//				i++;
-	//			} else {
-	//				stockPrices[k] = rightArray[j];
-	//				j++;
-	//			}
-	//			k++;
-	//		}
-	//
-	//		/* Copy remaining elements of L[] if any */
-	//		while (i < n1) {
-	//			stockPrices[k] = leftArray[i];
-	//			i++;
-	//			k++;
-	//		}
-	//
-	//		/* Copy remaining elements of R[] if any */
-	//		while (j < n2) {
-	//			stockPrices[k] = rightArray[j];
-	//			j++;
-	//			k++;
-	//		}
-	//	}
 
-	// Main function that sorts array[left...right] using merge() in Descending Order
-	//	public void sortInDescendingOrder(double[] stockPrices, int left, int right) {
-	//		if (left < right) {
-	//			// Find the middle point
-	//			int mid = (left + right) / 2;
-	//
-	//			// Sort first and second halves
-	//			sortInDescendingOrder(stockPrices, left, mid);
-	//			sortInDescendingOrder(stockPrices, mid + 1, right);
-	//
-	//			// Merge the sorted halves based on descending order
-	//			mergeForDescendingOrder(stockPrices, left, mid, right);
-	//		}
-	//	}
+	/*	public void mergeForDescendingOrder(double[] stockPrices, int left, int mid, int right) {
+			// Find sizes of two sub arrays to be merged
+			int n1 = mid - left + 1;
+			int n2 = right - mid;
+
+			// Create temporary arrays
+			double leftArray[] = new double[n1];
+			double rightArray[] = new double[n2];
+
+			// Copy data to temporary arrays
+			for (int i = 0; i < n1; ++i)
+				leftArray[i] = stockPrices[left + i];
+			for (int j = 0; j < n2; ++j)
+				rightArray[j] = stockPrices[mid + 1 + j];
+
+			// Merge the temporary arrays
+
+			// Initial indexes of first and second sub-arrays
+			int i = 0, j = 0;
+
+			// Initial index of merged sub-array array
+			int k = left;
+			while (i < n1 && j < n2) {
+				if (leftArray[i] >= rightArray[j]) {
+					stockPrices[k] = leftArray[i];
+					i++;
+				} else {
+					stockPrices[k] = rightArray[j];
+					j++;
+				}
+				k++;
+			}
+
+			// Copy remaining elements of L[] if any
+			while (i < n1) {
+				stockPrices[k] = leftArray[i];
+				i++;
+				k++;
+			}
+
+			// Copy remaining elements of R[] if any
+			while (j < n2) {
+				stockPrices[k] = rightArray[j];
+				j++;
+				k++;
+			}
+		}
+
+//	 Main function that sorts array[left...right] using merge() in Descending Order
+		public void sortInDescendingOrder(double[] stockPrices, int left, int right) {
+			if (left < right) {
+				// Find the middle point
+				int mid = (left + right) / 2;
+
+				// Sort first and second halves
+				sortInDescendingOrder(stockPrices, left, mid);
+				sortInDescendingOrder(stockPrices, mid + 1, right);
+
+				// Merge the sorted halves based on descending order
+				mergeForDescendingOrder(stockPrices, left, mid, right);
+			}
+		}
+	 */
 
 }
 
